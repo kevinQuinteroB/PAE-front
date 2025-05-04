9f5df41d6a723bb0d9bcbd7d2914ecff08357d09
Kevin Quintero <kevinstequintero@gmail.com>
2025-05-04 11:49:12 -0500

    HTML y CSS para pedidos por parte del operador

--- src/app/pedido-operador/pedido-operador.component.ts
+++ src/app/pedido-operador/pedido-operador.component.ts
@@ -11,6 +11,27 @@ export class PedidoOperadorComponent {
 
   @Output() cerrar = new EventEmitter<void>();
 
+  alimentos: string[] = ['Arroz', 'Frijoles', 'Leche', 'Pan', 'Huevos'];
+  fechas: string[] = ['2025-06-01', '2025-06-15', '2025-07-01'];
+
+  cantidades: number[] = [1];
+
+
+
+  textoFecha = "Fecha de Entrega"
+  hoy: string = new Date().toISOString().split('T')[0];
+
+  mostrarSelectorFecha = false;
+  fechaSeleccionada: string | null = null;
+
+  seleccionarFecha(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.fechaSeleccionada = input.value;
+    this.textoFecha = this.fechaSeleccionada.toString();
+    this.mostrarSelectorFecha = false;
+  }
+
+
   cantidadElementos: number = 1;
   cerrarPedido() { 
     this.cerrar.emit();
