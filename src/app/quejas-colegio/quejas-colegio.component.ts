b0d2fe9a2537f2159501c657f81e3bfffeebcbcc
jerson1210 <mantillajerson2@gmail.com>
2025-05-14 13:21:20 -0500

    quejas

--- src/app/quejas-colegio/quejas-colegio.component.ts
+++ src/app/quejas-colegio/quejas-colegio.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, ElementRef, ViewChild, AfterViewInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-quejas-colegio',
@@ -6,6 +7,56 @@ import { Component } from '@angular/core';
   templateUrl: './quejas-colegio.component.html',
   styleUrl: './quejas-colegio.component.css'
 })
-export class QuejasColegioComponent {
+export class QuejasColegioComponent implements AfterViewInit {
+
+  @ViewChild('deslizador') deslizador!: ElementRef;
+  @ViewChild('menuButton') menuButton!: ElementRef;
+  @ViewChild('closeButton') closeButton!: ElementRef;
+
+  constructor(private renderer: Renderer2, private router: Router) {}
+  
+  ngAfterViewInit(){
+    if (!this.menuButton || !this.closeButton || !this.deslizador) {
+      console.error('No se encontraron los elementos del menú.');
+      return;
+    }
+
+    this.menuButton.nativeElement.addEventListener('click', () => {
+      this.deslizador.nativeElement.style.transform = 'translateX(0)';
+    });
+
+    this.closeButton.nativeElement.addEventListener('click', () => {
+      this.deslizador.nativeElement.style.transform = 'translateX(-250px)';
+    });
+
+    this.renderer.listen('document', 'click', (event: Event) => {
+      const targetElement = event.target as HTMLElement;
+      if (this.deslizador && !this.deslizador.nativeElement.contains(targetElement) &&
+          !this.menuButton.nativeElement.contains(targetElement)) {
+        this.deslizador.nativeElement.style.transform = 'translateX(-250px)';
+      }
+    });
+  }
+
+    /* LOGICA DE ABRIR Y CERRAR COMPONENTES */
+
+    activeComponent: String | null = "colegio-busqueda";
+
+    setActiveComponent(component: string){
+      this.activeComponent = component;
+    }
+    isActive(component: string): boolean{
+      return this.activeComponent == component;
+    }
+    closeActiveComponent(){
+      this.activeComponent = "colegio-busqueda";
+    }
+  
+    /* SALIR DE LA SESION */
+  
+    salir() {
+      this.router.navigate(['/login']);
+    }
 
 }
+
