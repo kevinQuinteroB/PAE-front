900264b2a1a6f1aeaab89ddc63d5f7a7e1928420
jerson1210 <mantillajerson2@gmail.com>
2025-05-15 23:16:08 -0500

    vista

--- src/app/colegio-busqueda/colegio-busqueda.component.ts
+++ src/app/colegio-busqueda/colegio-busqueda.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Complaint} from '../models/queja';
+import { QuejaService } from '../services/queja.service';
+import { User } from '../models/user';
+import { UserService } from '../services/user.service';
+import { Order } from '../models/order';
 
 @Component({
   selector: 'app-colegio-busqueda',
@@ -7,5 +12,29 @@ import { Component } from '@angular/core';
   styleUrl: './colegio-busqueda.component.css'
 })
 export class ColegioBusquedaComponent {
+  user: User | null = null;
+  ordenes: Order[] = [];
+  token!: string;
+
+constructor(
+    private quejaService: QuejaService,
+    private userService: UserService, 
+  ){}
+
+
+   colegioRegistrado!: User;
+  ngOnInit() {
+  this.colegioRegistrado = this.userService.getUser();
+  console.log('Token:', this.colegioRegistrado.token);
+  this.quejaService.listarOrdenesPorEscuela(this.colegioRegistrado.token,this.colegioRegistrado.id).subscribe(
+          (data: Order[]) => {
+              this.ordenes = data;
+            },
+            (error) => {
+              console.error('Error al obtener inventario:', error);
+            }
+  );
+    
+  }
 
 }
--- src/app/models/order.ts
+++ src/app/models/order.ts
@@ -1,12 +1,28 @@
 export class Order {
-    idOrder?: number;  // opcional, porque se genera automáticamente
+  idOrder?: number;  // Opcional, generado automáticamente
+
   logistics!: {
     idUser: number;
   };
+
   school!: {
     idUser: number;
   };
+
   food!: {
     idFood: number;
+    name?: string;          // opcional si deseas mostrarlo
+    description?: string;   // opcional
   };
+
+  quantity!: number;
+
+  status!: string; // "enviada", "recibida", "cancelada"
+
+  deliveryDate!: string;  // formato ISO: "yyyy-mm-dd"
+
+  comment!: string;
+
+  creationDate!: string;  // también formato ISO
 }
+
