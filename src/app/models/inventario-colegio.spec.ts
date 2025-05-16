77fe388fce789f84bb987bfd0baa19cc8c1c8ebd
jerson1210 <mantillajerson2@gmail.com>
2025-05-16 15:28:02 -0500

    inventariocolegio

---
+++ src/app/models/inventario-colegio.spec.ts
@@ -0,0 +1,7 @@
+import { InventarioColegio } from './inventario-colegio';
+
+describe('InventarioColegio', () => {
+  it('should create an instance', () => {
+    expect(new InventarioColegio()).toBeTruthy();
+  });
+});
