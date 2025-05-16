489bdb2606afb010126203c4f44c216828fe287f
jerson1210 <mantillajerson2@gmail.com>
2025-05-15 22:28:48 -0500

    orden

---
+++ src/app/services/order.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Order } from '../models/order';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+    private api = 'http://localhost:8080/orderfood'; // Ajusta la URL base según tu backend
+
+
+  constructor(private http: HttpClient) {}
+
+    crearOrden(order: Order, token: string): Observable<Order> {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    });
+
+    return this.http.post<Order>(`${this.api}/create`, order, { headers });
+  }
+}
